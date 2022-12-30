6c9bef21200a3b89947b1c6612e2f1c8479ce7f2
Konrad Baran <45331390+barankonrad@users.noreply.github.com>
2022-12-31 00:43:44 +0100

    addNewClient procedure

--- QUERIES/CreateProcedures.sql
+++ QUERIES/CreateProcedures.sql
@@ -1,2 +1,11 @@
-SELECT *
-FROM Accounts
+CREATE PROCEDURE addNewClient
+@name NVARCHAR(100),
+@dateOfBirth DATE,
+@city NVARCHAR(100),
+@country NVARCHAR(100),
+@phoneNumber NVARCHAR(100)
+AS
+BEGIN
+    INSERT INTO Clients VALUES
+    (@name, @dateOfBirth, @city, @country, @phoneNumber)
+END
