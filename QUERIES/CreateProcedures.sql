c5ce3bb7ac563dddbdb0c7f2ccd8ea46f64020c9
Konrad Baran <45331390+barankonrad@users.noreply.github.com>
2022-12-31 00:58:18 +0100

    addNewClient procedure

--- QUERIES/CreateProcedures.sql
+++ QUERIES/CreateProcedures.sql
@@ -1,11 +1,17 @@
+DROP PROCEDURE addNewClient
+GO
 CREATE PROCEDURE addNewClient
 @name NVARCHAR(100),
 @dateOfBirth DATE,
 @city NVARCHAR(100),
 @country NVARCHAR(100),
-@phoneNumber NVARCHAR(100)
+@phoneNumber NVARCHAR(100),
+@allowPhoneTransfers BIT
 AS
 BEGIN
     INSERT INTO Clients VALUES
-    (@name, @dateOfBirth, @city, @country, @phoneNumber)
+    (@name, @dateOfBirth, @city, @country, @phoneNumber);
+    
+    INSERT INTO Preferences VALUES
+    (@@IDENTITY , NULL, @allowPhoneTransfers)
 END
